b3a8b3c85171a2d6be3f2f5cb5765ca7460d6f21
henrymj <henrymj@stanford.edu>
2020-09-03 15:22:39 -0700

    add SSRT computation

---
+++ compute_SSRT.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import argparse
+
+from stopsignalmetrics import SSRTmodel
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='simulation visualization')
+    parser.add_argument("--sim_types",
+                        nargs="+",
+                        default=['vanilla', 'guesses', 'graded_mu_go'])
+    parser.add_argument('--sim_dir',
+                        default='./Simulated_Data',
+                        help='location of simulated data')
+    parser.add_argument('--out_dir',
+                        default='./SSRT_metrics',
+                        help='location to save SSRT metrics')
+    parser.add_argument('--ssrt_model',
+                        default='all')
+    args = parser.parse_args()
+    return(args)
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+    ssrtmodel = SSRTmodel(model=args.ssrt_model)
+    for sim in args.sim_types:
+        sim_df = pd.read_csv('%s/%s.csv' % (args.sim_dir, sim))
+        metrics_df = ssrtmodel.fit_transform(sim_df, level='group')
+        metrics_df.to_csv('%s/%s.csv' % args.out_dir, sim)
