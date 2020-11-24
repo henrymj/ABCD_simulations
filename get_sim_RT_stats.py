24e82af1122a152d632622e07750d78cd6e9f567
henrymj <henrymj@stanford.edu>
2020-11-24 15:22:53 -0500

    slightly optimize sim RT script

--- get_sim_RT_stats.py
+++ get_sim_RT_stats.py
@@ -6,6 +6,6 @@ sim_files = glob('/oak/stanford/groups/russpold/users/henrymj/ABCD_simulations/s
 sim_rt_dict = {}
 for sim_file in sim_files:
     subid = sim_file.split('_')[-1].replace('.csv', '')
-    sim_rt_dict[subid] = pd.read_csv(sim_file).describe().loc[['mean', 'std'], 'goRT']
+    sim_rt_dict[subid] = pd.read_csv(sim_file)['goRT'].describe().loc[['mean', 'std']]
 
 pd.DataFrame(sim_rt_dict).T.add_prefix('sim_rt_').to_csv('sim_rt_meanStd.csv')
