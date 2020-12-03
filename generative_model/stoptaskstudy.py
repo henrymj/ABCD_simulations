5cd5f28b968b287824eece8541b58c684910eb68
poldrack <poldrack@gmail.com>
2020-12-03 13:52:09 -0800

    add command line args, and saving of data/params

--- generative_model/stoptaskstudy.py
+++ generative_model/stoptaskstudy.py
@@ -1,5 +1,8 @@
 # class for a single run of a stop study (i.e. a single subject)
 
+import argparse
+import json
+import os
 import numpy as np
 from trial import Trial
 from collections import namedtuple
@@ -39,7 +42,7 @@ class fixedSSD:
 
 # main study class
 class StopTaskStudy:
-    def __init__(self, SSDgenerator, params=None, **kwargs):
+    def __init__(self, SSDgenerator, outdir, params=None, **kwargs):
         if params is None:
             self.params = {
                 'mu': {'go': 0.3, 'stop': 0.5},
@@ -55,7 +58,9 @@ class StopTaskStudy:
             self.params = params
         for key, value in kwargs.items():
             self.params[key] = value
-
+        self.outdir = outdir
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
         self.SSDgenerator = SSDgenerator
         self.trialdata_ = None
         # create random subject code
@@ -94,10 +99,19 @@ class StopTaskStudy:
         self.trialdata_ = pd.DataFrame(data=trialdata)
         return(self.trialdata_)
 
+    def save_trialdata(self, save_params=True):
+        if self.trialdata_ is None:
+            raise Exception('Trialdata not yet computed')
+        outfile_stem = os.path.join(self.outdir, f'{self.params["subcode"]}')
+        self.trialdata_.to_csv(f'{outfile_stem}.csv')
+        with open(f'{outfile_stem}.json', 'w') as f:
+            json.dump(self.params, f)
+        
+
     def get_stopsignal_metrics(self):
         trialdata = self.trialdata_
         # clean up data for stopsignal metrics analysis
-        trialdata['subcode'] = study.params['subcode']
+        trialdata['subcode'] = self.params['subcode']
         trialdata['resp'] = trialdata.resp.astype(int)
         trialdata['correct_response'] = 1
         trialdata['block'] = 1
@@ -113,10 +127,40 @@ class StopTaskStudy:
         return(self.metrics_)
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='ABCD data simulator')
+    parser.add_argument('--paramfile', help='json file containing parameters')
+    parser.add_argument('--min_ssd', help='minimum SSD value', default=0)
+    parser.add_argument('--max_ssd', help='maximum SSD value', default=550)
+    parser.add_argument('--ssd_step', help='SSD step size', default=50)
+    parser.add_argument('--n_subjects', nargs='+',
+                        help='number of subjects to simulate', default=1)
+    parser.add_argument('--out_dir',
+                        default='./simulated_data/pseudosubjects',
+                        help='location to save simulated data')
+    args = parser.parse_args()
+    return(args)
+
+
 if __name__ == '__main__':
-    ssd = fixedSSD(np.arange(0, 550, 50))
-    study = StopTaskStudy(ssd)
+    args = get_args()
+    print(f'simulating stop task for {args.n_subjects} subjects')
+    if args.paramfile is not None:
+        with open(args.paramfile) as f:
+            params = json.load(f)
+    else:
+        params = None
+    print(params)
+
+    ssd = fixedSSD(np.arange(args.min_ssd, args.max_ssd + args.ssd_step, args.ssd_step))
+    study = StopTaskStudy(ssd, args.out_dir)
+
+    # save some extra params for output to json
+    study.params['args'] = args.__dict__
+    study.params['pwd'] = os.getcwd()
+
     trialdata = study.run()
+    study.save_trialdata()
 
     # summarize data - go trials are labeled with SSD of -inf so that
     # they get included in the summary
