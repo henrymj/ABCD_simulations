4a01d00cb4ff9b3675893ce7d69c58f719b76cda
poldrack <poldrack@gmail.com>
2020-12-04 14:30:48 -0800

    move ssd generators to separate file

--- generative_model/stoptaskstudy.py
+++ generative_model/stoptaskstudy.py
@@ -8,83 +8,14 @@ from trial import Trial, init_default_params, fix_params
 from collections import namedtuple
 import pandas as pd
 from stopsignalmetrics import StopData, SSRTmodel, PostStopSlow, Violations, StopSummary
-
+from ssd import trackingSSD, fixedSSD
 Trialdata = namedtuple('Trialdata', 'trialtype, SSD, rt, resp, correct')
-
-
-# SSD generators
-class fixedSSD:
-    def __init__(self, SSDs, **kwargs):
-        """[summary]
-
-        Args:
-            SSDs (list): a list of SSDs (in which case they are presented with equal frequency)
-                TBD(?): or a dictionary containing the SSDs and their relative frequency
-        """
-        self.SSDs = np.array(SSDs)
-
-    def generate(self, n_stop_trials):
-        """generator for fixed SSD design
-        - if number of SSDs is not a multiple of n_stop_trials, then extra SSDs are randomly selected
-
-        Args:
-            n_stop_trials (int): number of stop trials
-
-        Yields:
-            [type]: [description]
-        """
-        num_repeats = np.ceil(n_stop_trials / len(self.SSDs))
-        SSDlist = np.repeat(self.SSDs, num_repeats)
-        np.random.shuffle(SSDlist)
-        SSDlist = SSDlist[:n_stop_trials]
-        for i in range(n_stop_trials):
-            yield SSDlist[i]
-
-
-# based on the ABCD tracking algorithm
-class trackingSSD:
-    def __init__(self, starting_ssd=50, step_size=50, min_ssd=0, max_ssd=550):
-        self.starting_ssd = starting_ssd
-        self.step_size = step_size
-        self.min_ssd = min_ssd
-        self.max_ssd = max_ssd
-
-        self.SSD = starting_ssd
-
-    def update(self, SSD, success):
-        """[summary]
-
-        Args:
-            SSD (int): current sssd
-            success (boolean): success or failure
-
-        Returns:
-            SSD (int): updated SSD
-        """
-        if SSD is None:
-            SSD = self.starting_ssd
-        elif success:
-            SSD = SSD + self.step_size
-        else:
-            SSD = SSD - self.step_size
-
-        if SSD < self.min_ssd:
-            self.SSD = self.min_ssd
-        elif SSD > self.max_ssd:
-            self.SSD = self.max_ssd
-        else:
-            self.SSD = SSD
-
-        return(self.SSD)
 
 
 # main study class
 class StopTaskStudy:
     def __init__(self, SSDgenerator, outdir, params=None, **kwargs):
-        if params is None:
-            self.params = init_default_params()
-        else:
-            self.params = params
+        self.params = init_default_params() if params is None else params
         for key, value in kwargs.items():
             self.params[key] = value
 
@@ -96,6 +27,7 @@ class StopTaskStudy:
         self.SSDgenerator = SSDgenerator
         self.trialdata_ = None
         self.metrics_ = None
+
         # create random subject code
         if 'subcode' not in self.params:
             self.params['subcode'] = np.random.randint(10e12)
