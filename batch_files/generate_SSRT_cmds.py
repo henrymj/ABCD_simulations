ccfe92e2f5238d0e7f0d654cb59983238d19df04
henrymj <henrymj@stanford.edu>
2020-12-05 10:48:59 -0500

    add in paths to SSRT command

--- batch_files/generate_SSRT_cmds.py
+++ batch_files/generate_SSRT_cmds.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
 #SBATCH --mail-user=henrymj@stanford.edu
 #SBATCH -N 1 # number of nodes requested - set to ceil(n rows in command script / 24)
 #SBATCH -n %d # total number of mpi tasks requested - set to n rows in command script
-#SBATCH -p normal
+#SBATCH -p russpold,normal
 # Job Submission
 #-----------
 export PYTHONPATH=""
@@ -120,6 +120,10 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate py3-env
 
 '''
+    ABCD_LOC = '/oak/stanford/groups/russpold/users/henrymj/ABCD_simulations/abcd_data'
+    SIM_LOC = '/oak/stanford/groups/russpold/users/henrymj/ABCD_simulations/simulated_data/individual_data'
+    SSRT_LOC = '/oak/stanford/groups/russpold/users/henrymj/ABCD_simulations/ssrt_metrics/individual_metrics'
+
     nsubs_per_job = 48
     njobs_per_node = 24
     nlines = 0
@@ -131,7 +135,7 @@ conda activate py3-env
         if end_idx > len(remaining_subs):
             end_idx = len(remaining_subs)
         substr = ' '.join(remaining_subs[start_idx:end_idx])
-        file_str += (f'eval "python ../../scripts/compute_indiv_SSRTs.py --subjects {substr}" &\n')
+        file_str += (f'eval "python ../../scripts/compute_indiv_SSRTs.py --abcd_dir {ABCD_LOC} --sim_dir {SIM_LOC} --out_dir {SSRT_LOC} --subjects {substr}" &\n')
         nlines += 1
         if nlines == 24:
             with open(sher_sim_file % batch_counter, 'w') as f:
