1cbbc389ae47ccd8902c831144fb7bd16a02ef41
poldrack <poldrack@gmail.com>
2020-12-07 08:31:36 -0800

    update function usage

--- generative_model/test_pyabc.py
+++ generative_model/test_pyabc.py
@@ -1,12 +1,12 @@
-from pyABC import stopsignal_model
+from pyABC import stopsignal_model_basic
 
 
 def test_stopsignal_model():
-    params = {'mu_delta_incorrect': 0.10386248711279868,
-              'mu_go': 0.11422675271126799,
-              'mu_stop_delta': 0.7850423871897488,
-              'noise_sd': 3.1238287051634597,
-              'nondecision': 50}
+    model_parameters = {'mu_delta_incorrect': 0.10386248711279868,
+                        'mu_go': 0.11422675271126799,
+                        'mu_stop_delta': 0.7850423871897488,
+                        'noise_sd': 3.1238287051634597,
+                        'nondecision': 50}
 
-    simulation = stopsignal_model(params)
+    simulation = stopsignal_model_basic(model_parameters)
     assert simulation is not None
