93aac33db20d8d252781617a7c87da42a3470d58
poldrack <poldrack@gmail.com>
2020-12-07 07:22:50 -0800

    add cmd line args

--- generative_model/pyABC.py
+++ generative_model/pyABC.py
@@ -31,25 +31,32 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import logging
+import argparse
 
 from ssd import fixedSSD
 from stoptaskstudy import StopTaskStudy
 # %matplotlib inline
 
+def get_args():
+    parser = argparse.ArgumentParser(description='fit stop model params using ABC-SMC')
+    parser.add_argument('--study', help='study label to define dataset to be fit')
+    parser.add_argument('--nthreads', help='max # of multiprocessing threads', default=8)
+    parser.add_argument('--max_populations', help='max # of ABC populations', default=12)
+    parser.add_argument('--min_epsilon', help='epsilon threshold for ABC', default=.1)
+    parser.add_argument('--min_ssd', help='minimum SSD value', default=0)
+    parser.add_argument('--max_ssd', help='maximum SSD value', default=550)
+    parser.add_argument('--ssd_step', help='SSD step size', default=50)
+    parser.add_argument('--random_seed', help='random seed', type=int)
+    parser.add_argument('--p_guess_go', help='p_guess on go trials (default None)')
+    parser.add_argument('--debug', help='turn on debugging output from ABC', action='store_true')
+    parser.add_argument('--verbose', help='turn on verbose output', action='store_true')
+    parser.add_argument('--stop_guess_ABCD', help='use SSD-dependent guessing based on ABCD data', action='store_true')
 
+    parser.add_argument('--guess_param_file', default='exgauss_params.json',
+                        help='file with exgauss params for guesses')
+    # parser.add_argument('--tracking', help='use tracking algorithm', action='store_true')
+    return parser.parse_args()
 
-# %%
-# set to True for debugging outputs
-
-debug = False
-
-if debug:
-    df_logger = logging.getLogger('Distance')
-    df_logger.setLevel(logging.DEBUG)
-
-
-
-# %%
 # create a single-layer metrics dict for output
 # since the pickler can't handle multilevel dicts
 def cleanup_metrics(metrics):
@@ -58,7 +65,7 @@ def cleanup_metrics(metrics):
     del metrics['SSRT']
     return(metrics)
 
-# %%
+
 # create the main model function
 # takes in a dict of model parameters
 # returns a dict of peformance statistics
@@ -67,6 +74,7 @@ def stopsignal_model(parameters):
     paramfile = 'params.json'
     with open(paramfile) as f:
             params = json.load(f)
+
     # install the parameters from the simulation
     parameters['nondecision'] = int(parameters['nondecision'])
     params['mu']['go'] = parameters['mu_go']
@@ -82,8 +90,6 @@ def stopsignal_model(parameters):
     #        p_guess = pd.read_csv(args.p_guess_file, index_col=0)
     #        assert 'SSD' in p_guess.columns and 'p_guess' in p_guess.columns
 
-    #    if args.random_seed is not None:
-    #        np.random.seed(args.random_seed)
 
     min_ssd, max_ssd, ssd_step = 0, 550, 50
     ssd = fixedSSD(np.arange(min_ssd, max_ssd + ssd_step, ssd_step))
@@ -106,57 +112,70 @@ def stopsignal_model(parameters):
 
     return(results)
 
-parameter_prior = Distribution(mu_go=RV("uniform", .1, .5),
-                               mu_stop_delta=RV("uniform", 0, 1),
-                              mu_delta_incorrect=RV("uniform", 0, 0.2),
-                              noise_sd=RV("uniform", 2, 5),
-                              nondecision=RV("uniform", 25, 75))
-parameter_prior.get_parameter_names()
+if __name__ == '__main__':
+    args = get_args()
+    print(f'fitting stop task for {args.study}')
+
+    if args.random_seed is not None:
+        print('WARNING: Fixing random seed')
+        np.random.seed(args.random_seed)
+
+    # set numexpr thread limits
+    os.environ['NUMEXPR_MAX_THREADS'] = str(args.nthreads)
+    os.environ['NUMEXPR_NUM_THREADS'] = str(args.nthreads)
+
+    max_populations = int(args.max_populations)
+    min_epsilon = float(args.min_epsilon)
+
+    if args.debug:
+        df_logger = logging.getLogger('Distance')
+        df_logger.setLevel(logging.DEBUG)
 
 
-# %%
-# testing the model function to make sure it works...
+    # set up priors for ABC
+    # these values are based on some hand-tweaking using the in-person dataset
+    parameter_prior = Distribution(mu_go=RV("uniform", .1, .5),
+                                mu_stop_delta=RV("uniform", 0, 1),
+                                mu_delta_incorrect=RV("uniform", 0, 0.2),
+                                noise_sd=RV("uniform", 2, 5),
+                                nondecision=RV("uniform", 25, 75))
 
-test_model = False
-if test_model:
-    params={'mu_delta_incorrect': 0.10386248711279868,
-    'mu_go': 0.11422675271126799,
-    'mu_stop_delta': 0.7850423871897488,
-    'noise_sd': 3.1238287051634597,
-    'nondecision':50}
+    parameter_prior.get_parameter_names()
 
-    simulation = stopsignal_model(params)
-    simulation
+    # use an adaptive distance function
+    # which deals with the fact that different outcome measures have
+    # different scales - automatically scales to them
 
+    distance_adaptive = pyabc.AdaptivePNormDistance(
+        p=2, scale_function=pyabc.distance.root_mean_square_deviation)
 
-# %%
-# use an adaptive distance function
-# which deals with the fact that different outcome measures have
-# different scales - automatically scales to them
-
-distance_adaptive = pyabc.AdaptivePNormDistance(
-    p=2, scale_function=pyabc.distance.root_mean_square_deviation)
-
-abc = ABCSMC(stopsignal_model, parameter_prior, distance_adaptive,
-    acceptor = pyabc.UniformAcceptor(use_complete_history=True))
+    # set up the sampler
+    # use acceptor which seems to improve performance with adaptive distance
+    abc = ABCSMC(stopsignal_model, parameter_prior, distance_adaptive,
+        acceptor = pyabc.UniformAcceptor(use_complete_history=True))
 
 
-# %%
-# set up the database for the simulation
-db_path = 'sqlite:///adaptive_distance.db' # pyabc.create_sqlite_db_id(file_="./adaptive_distance.db")
+    # set up the database for the simulation
+    db_path = f'sqlite:///results/{args.study}_adaptive_distance.db' # pyabc.create_sqlite_db_id(file_="./adaptive_distance.db")
 
-# observed metrics specified here by hand.  should instead store metrics to json
-# and load those
-observed_data = {'mean_go_RT': 455.367, 'mean_stopfail_RT': 219.364, 'go_acc': .935}
+    # load the data to be fitted
+    # this should contain the output from stopsignalmetrics, including:
+    # {'mean_go_RT': , 'mean_stopfail_RT': , 'go_acc': }
+    with open(f'data/data_{args.study}.json') as f:
+        observed_data = json.load(f)
 
-# load presp data from txt file
-observed_presp = pd.read_csv('presp_by_ssd_inperson.txt',  delimiter=r"\s+", index_col=0)
-for i, value in enumerate(observed_presp.presp.values):
-    observed_data[f'presp_{i}'] = value
+    # observed_data = {'mean_go_RT': 455.367, 'mean_stopfail_RT': 219.364, 'go_acc': .935}
 
-# initiatize database and add observed data
-abc.new(db_path, observed_data)
+    # load presp data from txt file
+    observed_presp = pd.read_csv(f'data/presp_by_ssd_{args.study}.txt',  delimiter=r"\s+", index_col=0)
+    for i, value in enumerate(observed_presp.presp.values):
+        observed_data[f'presp_{i}'] = value
 
-# %%
-# run the model
-history = abc.run(minimum_epsilon=.1, max_nr_populations=15,)
+    # initiatize database and add observed data
+    abc.new(db_path, observed_data)
+
+    # %%
+    # run the model
+    history = abc.run(
+        minimum_epsilon=min_epsilon,
+        max_nr_populations=max_populations,)
