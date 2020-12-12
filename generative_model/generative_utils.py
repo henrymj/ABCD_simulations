ebd7c40a0b877fe6d174e41cf36c1bc31072ec46
poldrack <poldrack@gmail.com>
2020-12-12 12:25:46 -0800

    delete SSRT

--- generative_model/generative_utils.py
+++ generative_model/generative_utils.py
@@ -3,8 +3,8 @@
 # create a single-layer metrics dict for output
 # since the pickler can't handle multilevel dicts
 def cleanup_metrics(metrics):
-    for k in metrics['SSRT']:
-        metrics['SSRT_' + k] = metrics['SSRT'][k]
+#    for k in metrics['SSRT']:
+#        metrics['SSRT_' + k] = metrics['SSRT'][k]
     del metrics['SSRT']
     return(metrics)
 
