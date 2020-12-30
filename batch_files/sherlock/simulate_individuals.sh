87c08a4723d689a084842e5cea9275915ce9da6e
henrymj <henrymj@stanford.edu>
2020-12-30 13:31:50 -0500

    fix sim shell script to eval sbatch command

--- batch_files/sherlock/simulate_individuals.sh
+++ batch_files/sherlock/simulate_individuals.sh
@@ -2,6 +2,6 @@ for mu_suffix in SSRTscale-85 SSRTscale-25 SSRTscale-0 SSRTscale-0_RTscale-0
 do
     for iter in {0..4}
     do
-        echo "${mu_suffix}/sherlock_run_sims_iter${iter}.batch"
+        eval "sbatch ${mu_suffix}/sherlock_run_sims_iter${iter}.batch"
     done
 done
